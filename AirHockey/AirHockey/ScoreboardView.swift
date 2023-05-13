eba21c3aacef453e247da90db12ce1a6c0c6c07d
lyqm09 <54779158+lyqm09@users.noreply.github.com>
2023-05-13 19:52:22 +0200

    add victory

--- AirHockey/AirHockey/ScoreboardView.swift
+++ AirHockey/AirHockey/ScoreboardView.swift
@@ -11,24 +11,39 @@ struct ScoreboardView: View {
     @ObservedObject var bluetooth: BLEAirHockey;
     var fontSize: CGFloat = 200;
     var body: some View {
-        HStack {
+        @State var s1 = bluetooth.score[0];
+        var s2 = bluetooth.score[1];
+        if(s1 == 0xFE) {
+            var winner = s2;
+            VStack {
+                Text("Victoire")
+                    .font(.system(size: 26.0))
+                    .foregroundColor(.white)
+            }
+            .background(s2 == 0x00 ? Color.blue : Color.red)
             
-            Spacer()
-            
-            Text("\(bluetooth.score[0])")
-                .font(.system(size: fontSize))
-                .foregroundColor(.blue)
-            
-            Spacer()
-            Divider()
-                .frame(height: fontSize*1.5)
-            Spacer()
-            
-            Text("\(bluetooth.score[1])")
-                .font(.system(size: fontSize))
-                .foregroundColor(.red)
-            
-            Spacer()
+        } else {
+            HStack {
+                
+                Spacer()
+                
+                Text("\(s1)")
+                    .font(.system(size: fontSize))
+                    .foregroundColor(.blue)
+                
+                Spacer()
+                Divider()
+                    .frame(height: fontSize*1.5)
+                Spacer()
+                
+                Text("\(s2)")
+                    .font(.system(size: fontSize))
+                    .foregroundColor(.red)
+                
+                Spacer()
+                
+                
+            }
         }
     }
 }
