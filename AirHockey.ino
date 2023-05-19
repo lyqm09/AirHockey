ea94e8cb0feb13aea165e5c3f0834a0e0dd6d5a0
lyqm09 <54779158+lyqm09@users.noreply.github.com>
2023-05-19 10:43:54 +0200

    Add files via upload

--- AirHockey.ino
+++ AirHockey.ino
@@ -20,9 +20,8 @@
 #define OUTPUT_LEDB 33
 
 
-#define MAX_POINTS 0x0A;
+#define MAX_POINTS 0x05;
 uint8_t score[2];
-bool hasMetal = false;
 bool bonus = false;
 uint8_t red = LOW;
 uint8_t green = LOW;
@@ -34,14 +33,14 @@ BLECharacteristic *pOutputChar;
 void sendScoreboard() {
   outputData[0] = score[0];
   outputData[1] = score[1];
-  Serial.printf("Scoreboard : %02x - %02x\r\n", outputData[0], outputData[1]);
 
   pOutputChar->setValue((uint8_t *)outputData, 2);
   pOutputChar->notify();
+  Serial.printf("Scoreboard : %02x - %02x\r\n", outputData[0], outputData[1]);
 }
 
 void updateGame() {
-  
+
   bool isWin1 = score[0] >= MAX_POINTS;
   bool isWin2 = score[1] >= MAX_POINTS;
 
@@ -148,7 +147,7 @@ void setup() {
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(true);
-  pAdvertising->setMinPreferred(0x06);  
+  pAdvertising->setMinPreferred(0x06);
   pAdvertising->setMinPreferred(0x12);
   BLEDevice::startAdvertising();
 
@@ -161,35 +160,63 @@ void setup() {
   pinMode(INPUT_PIN2, INPUT);
 
   //LEDS
-  pinMode(OUTPUT_LEDR, OUTPUT); 
+  pinMode(OUTPUT_LEDR, OUTPUT);
   pinMode(OUTPUT_LEDG, OUTPUT);
   pinMode(OUTPUT_LEDB, OUTPUT);
 
   xTaskCreatePinnedToCore(bonusAsyncTask, "BonusAsyncTask", 1024, NULL, 1, NULL, ARDUINO_RUNNING_CORE);
 }
 
+int player1_threshold = 0;
+int player2_threshold = 0;
+int player2_off = 0;
+int LIMIT = 10000;
+
 void loop() {
   // put your main code here, to run repeatedly:
-  int player1 = digitalRead(INPUT_PIN1);
-  int player2 = digitalRead(INPUT_PIN2);
+  int player1_entry = digitalRead(INPUT_PIN1);
+  int player2_entry = digitalRead(INPUT_PIN2);
+
+  if(player1_entry == HIGH) {
+    if (player1_threshold < LIMIT + 15) {
+      player1_threshold += 1;
+    }
+  } else {
+    player1_threshold = 0;
+  }
+
+  if(player2_entry == HIGH) {
+    if (player2_threshold < LIMIT + 15) {
+      player2_threshold += 1;
+    }
+  } else {
+    player2_off += 1;
+  }
+  if (player2_off > 200) {
+    player2_threshold = 0;
+    player2_off = 0;
+  }
+
+
+   if(Serial.available()>0) {
+     int incomingData = Serial.parseInt();
+     if (incomingData == 1) {
+       player1_threshold = LIMIT;
+     } else if (incomingData == 2) {
+       player2_threshold = LIMIT;
+     }
+   }
 
   digitalWrite(OUTPUT_LEDR, red);
   digitalWrite(OUTPUT_LEDG, green);
-  digitalWrite(OUTPUT_LEDB, blue); 
+  digitalWrite(OUTPUT_LEDB, blue);
 
-  if(player1 == HIGH && !hasMetal) {
-    hasMetal = true;
-    addScore(bonus? 0 : 1, bonus? -1 : 1);
-  } else if(player1 == LOW && hasMetal) {
-    hasMetal = false;
-    delay(1000);
-  }
-  if(player2 == HIGH) {
-    hasMetal = true;
+
+  if(player1_threshold == LIMIT) {
     addScore(bonus? 1 : 0, bonus? -1 : 1);
-  } else if(player2 == LOW && hasMetal) {
-    hasMetal = false;
-    delay(1000);
+  }
+  if(player2_threshold == LIMIT) {
+    addScore(bonus? 0 : 1, bonus? -1 : 1);
   }
 
 }
